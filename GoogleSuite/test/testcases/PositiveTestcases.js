d171e8a0caa92b497e072c4c56d5696165bc591a
Bharathi Kannan B <bharathikannanbtechit@gmail.com>
2021-03-26 07:27:37 +0530

    Update PositiveTestcases.js

--- GoogleSuite/test/testcases/PositiveTestcases.js
+++ GoogleSuite/test/testcases/PositiveTestcases.js
@@ -4,6 +4,7 @@ import SearchLandingPage from'../pageobject/SearchLandingPage';
 // Testcase 01 - To validate the Search functionality in GoogleHomePage
 
 describe('Testcase 01 Search functionality', function() {
+	try {
     before(function()
     {
         HomePage.OpenURL();
@@ -16,13 +17,19 @@ describe('Testcase 01 Search functionality', function() {
     }),
     it('Validate Search Result', function () {
         const result= SearchLandingPage.searchResult().getText();
+	browser.saveScreenshot('./screenshot/screen/Testcase01.png');
         assert.strictEqual(result, "Apple (India)");
+	console.log("Search functionality validated");
     }),
+	    } catch (error) {
+    console.log(error.name);
+}
 });
     
    // Testcase 02 - To validate the I'm Feeling Lucky functionality in GoogleHomePage
 
 describe('Testcase 02 - I'm Feeling Lucky functionality', function() {
+	 try {
     before(function()
     {
         HomePage.OpenURL();
@@ -35,13 +42,19 @@ describe('Testcase 02 - I'm Feeling Lucky functionality', function() {
     }),
     it('Validate Search Result', function () {
         const result= browser.getTitle();
+	browser.saveScreenshot('./screenshot/screen/Testcase02.png');
         assert.strictEqual(result, "https://www.apple.com/in/");
+	console.log("I'm Feeling Lucky functionality validated");
     }),
+	    } catch (error) {
+    console.log(error.name);
+}
 });
 
  // Testcase 03 - To verify and validate language change functionality in GoogleHomePage
 
 describe('Testcase 03 - Language UI functionality', function() {
+	try {
     before(function()
     {
         HomePage.OpenURL();
@@ -51,13 +64,19 @@ describe('Testcase 03 - Language UI functionality', function() {
     }),
     it('Validate Language', function () {
         const result=  HomePage.LanguageButton().getText();
+	browser.saveScreenshot('./screenshot/screen/Testcase03.png');
         assert.strictEqual(result, "English");
+	console.log("Language change UI functionality validated");
     }),
+	    } catch (error) {
+    console.log(error.name);
+}
 });
     
   // Testcase 04 & 05 - To validate Keyboard and Mouse Actions in GoogleHomePage 
 
-describe('Testcase 03 - Language UI functionality', function() {
+describe('Testcase 04 - Keyboard and Mouse Actions', function() {
+	try {
     before(function()
     {
         HomePage.OpenURL();
@@ -79,6 +98,11 @@ describe('Testcase 03 - Language UI functionality', function() {
     }),
          it('Validate Mouse Action', function () {
         const result=  HomePage.SearchBox().getText();
+	browser.saveScreenshot('./screenshot/screen/Testcase0405.png');
         assert.strictEqual(result, "Apple");
+	console.log("Keyboard and Mouse actions validated");
     }),
+	    } catch (error) {
+    console.log(error.name);
+}
 });  
